bae91b8c754296a07c425648190da6ae2050e06f
EmilieCherel <cherel.emi@gmail.com>
2023-03-27 22:38:54 +0200

    Mise en place de mongoose, création du model Chicken

---
+++ models/chicken.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const chickenSchema = mongoose.Schema({
+  name: { type: String, require: true },
+  birthday: { type: Date },
+  weight: { type: Number, require: true },
+  steps: { type: Number, default: 0 },
+  isRunning: { type: Boolean, default: false },
+});
+
+module.exports = mongoose.model("Chicken", chickenSchema);
